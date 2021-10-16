7202c5c5ce84d433d7746cfa7758bc3b012cad3b
Anderson Ribeiro <anderson_ribeiro.a@hotmail.com>
2021-10-16 16:49:29 -0300

    Adiciona imagem, descricao e retirar cor de link

--- src/app/model/filme.ts
+++ src/app/model/filme.ts
@@ -1,10 +1,14 @@
 export class Filme {
 
-    constructor(nome?: string, link?: string) {
+    constructor(nome?: string, link?: string, urlImage?: string, description?: string) {
         this.nome = nome;
         this.link = link;
+        this.urlImage = urlImage;
+        this.description = description;
     }
 
-    nome:string | undefined;
-    link:string | undefined;
+    nome: string | undefined;
+    link: string | undefined;
+    urlImage: string | undefined;
+    description: string | undefined
 }
